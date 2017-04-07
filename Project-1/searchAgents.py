7969b60106d117565a4acf5da1ba50a22d0ac3a8
TianxiaoHu <hutianxiao_fdu@126.com>
2017-04-08 00:54:03 +0800

    Try Question 7: Eating All The Dots
    Use the maximum of Manhattan distance as h(n) and get a score of
    3/5(needs further improvement)

--- Project-1/searchAgents.py
+++ Project-1/searchAgents.py
@@ -464,8 +464,8 @@ def foodHeuristic(state, problem):
     problem.heuristicInfo['wallCount']
     """
     position, foodGrid = state
-    "*** YOUR CODE HERE ***"
-    return 0
+    foodDistance = [util.manhattanDistance(position, food) for food in foodGrid.asList()]
+    return 0 if len(foodDistance) == 0 else max(foodDistance)
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
